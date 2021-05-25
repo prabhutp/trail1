647f0215fedcd5f81f2b7a82fb83733591903f19
prabhutp <50525959+prabhutp@users.noreply.github.com>
2021-05-25 13:14:11 +0530

    Update settings.py

--- trail1/settings.py
+++ trail1/settings.py
@@ -98,8 +98,9 @@ DATABASES = {
 }
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
